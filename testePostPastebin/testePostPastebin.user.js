d3c6060cc22d5ce2bd241907420abbf5818f1035
Micalevisk <mllc@icomp.ufam.edu.br>
2016-12-05 16:13:04 -0400

    dados externos "privados"

--- testePostPastebin/testePostPastebin.user.js
+++ testePostPastebin/testePostPastebin.user.js
@@ -8,10 +8,13 @@
 // @version			1.04-2
 // @run-at			document-end
 // @grant			GM_xmlhttpRequest
+// @grant       	GM_getResourceText
+// @resource    	authentication file:///C:/Users/user/AppData/Roaming/Mozilla/Firefox/Profiles/xwt25znr.default/gm_scripts/_private.js
 // ==/UserScript==
 
 //	TODO	como criar um post privado
 //	TODO	para "editar" um paste, dado o id de um já existente, obter content raw, criar outro com o conteúdo acrescido de informações e deletar o primeiro.
+
 
 
 /// TESTES
@@ -21,48 +24,59 @@ const DELET = Boolean(false)
 
 
 
+/////////////// [ PRIVATE DATA ] ///////////////
+eval(GM_getResourceText("authentication"));
+const AUTH = pastebin_privateData;
+API_USER_NAME= AUTH.username;
+API_USER_PASSWORD= AUTH.userpass;
+API_DEV_KEY= AUTH.userdevkey;
+////////////////////////////////////////////////
+
+
 /////////////// [ GLOBAL CONFIGS ] ///////////////
-API_USER_NAME= "micalevisk"
-API_USER_PASSWORD= "<PASSWD>"
-
-
 /// [ Required Parameters ]
 API_PASTE_CODE = encodeURI("Some really good code A\nBC");
-API_DEV_KEY	= "<DEVKEY>"
 API_OPTION 	= {
 	post: "paste", // to create a new paste															[ api_dev_key, api_user_name, api_user_password ]
 	get_raw: "show_paste", // to get raw paste output of users pastes including 'private' pastes	[ api_dev_key, api_user_key, api_paste_key ]
 	list: "list", // to list pastes created by a user												[ api_dev_key, api_user_key ]
 	delete_raw: "delete" // to delete a paste created by a user										[ api_dev_key, api_user_key, api_paste_key ]
-}
+};
 URL = {
 	post: "http://pastebin.com/api/api_login.php",
 	get_raw: "http://pastebin.com/api/api_raw.php",
-	list: "http://pastebin.com/api/api_post.php",
-}
-
+	list: "http://pastebin.com/api/api_post.php"
+};
 
 /// [Optional Parameters]
 API_PASTE_NAME			= encodeURI("some cool title.here");
-API_PASTE_FORMAT		= "text"
+API_PASTE_FORMAT		= "text";
 API_PASTE_PRIVATE		= {
 	public: "0",
 	unlisted: "1",
 	private: "2"
-}
+};
 API_PASTE_EXPIRE_DATE	= {
 	never: "N", // (only allowed in combination with api_user_key, as you have to be logged into your account to access the paste)
 	ten_min: "10M",
 	one_hour: "1H",
 	one_day: "1D",
-	one_month: "1M",
-}
+	one_month: "1M"
+};
 /////////////////////////////////////////////////
 
 
 
+/////////////////// [ MAIN ] ////////////////////
 function getAPI_USER_KEY(callback, callback_arg){
 	if(!callback) return;
+
+	/*
+	var a = Array.prototype.slice.call(arguments),	// args
+	e = a[0],	// expression
+	c = a[1],	// context
+	t = a[2];	// type
+	*/
 
 	/// http://pastebin.com/api#7
 	var params = "api_dev_key="+API_DEV_KEY + "&api_user_name="+API_USER_NAME + "&api_user_password="+API_USER_PASSWORD;
@@ -83,6 +97,8 @@ function getAPI_USER_KEY(callback, callback_arg){
 	});
 
 }
+/////////////////////////////////////////////////
+
 
 
 ////////////////////////////////////////////////// [ POST MEMBER LOGIN ] /////////////////////////////////////////////////
