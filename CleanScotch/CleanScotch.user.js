6dfffcd245479fe2198e031155907a21fdb28787
micalevisk <mllc@icomp.ufam.edu.br>
2017-03-02 12:37:54 -0400

    new simple script

---
+++ CleanScotch/CleanScotch.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        CleanScotch
+// @description esconde elementos irrelevantes
+// @namespace   https://scotch.io
+// @author      Micael Levi
+// @language    pt-br
+// @include     *://scotch.io/*
+// @version     0.02-3
+// @require		https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// @grant       document-end
+// ==/UserScript==
+(function($){
+	$('.sidebar').remove(); // barra lateral direita
+	$('.super-side').remove(); // barra lateral esquerda
+	$('div.page-friend').remove(); // barra flutuante de redes sociais
+	$('header.site-header').remove(); $('div.super-search').remove(); // barra de pesquisa
+	$('.single-top-scotcherisment').remove(); // espaço vazio do topo
+	$('.clearfix').remove(); // related course
+})(jQuery)
