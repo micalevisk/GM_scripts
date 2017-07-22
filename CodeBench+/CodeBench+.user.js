fa6bf44404bcfcca72673a6aa2c2eddfd622191d
Micael Levi <mllc@icomp.ufam.edu.br>
2017-07-21 22:39:56 -0400

    refatorada de levis && fixed detalhes

--- CodeBench+/CodeBench+.user.js
+++ CodeBench+/CodeBench+.user.js
@@ -2,47 +2,47 @@
 // @name        CodeBench+
 // @namespace   https://github.com/micalevisk/GM_scripts/tree/master/CodeBench+
 // @supportURL	https://github.com/micalevisk
-// @description	Aumenta em 100% as chances de a carta sorteada ser a carta desejada com maior valor. (ES6)
+// @description	Aumenta em 100% as chances de a carta sorteada ser a carta desejada com maior valor possível. (ES6)
 // @author      Micael Levi L. C.
 // @language    pt-br
-// @include     *//codebench.icomp.ufam.edu.br/index.php?r=trabalho%2Fview&id=*&turma=*
-// @version     0.14-5
+// @include     *//codebench.icomp.ufam.edu.br/index.php?r=*view&id=*&turma=*
+// @version     0.21-7
 // @grant       none
 // @run-at		document-end
 // ==/UserScript==
 
 (function(){
-	if($("a[id^=menu_submeter_codigo_]").length)//verifica se o trabalho ainda pode ser submetido
+	if($("a[id^=menu_submeter_codigo_]").length)///verifica se o trabalho ainda pode ser submetido
 		initSistema();
-		// console.log.apply(console, ['%c Sistema Em Construção', 'font:1.5em/1.5 italic comic sans,chalkboard,tscu_comic,fantasy;color:hotpink;']);
+		// console.log.apply(console, ['%c Sistema Em Manutenção', 'font:1.5em/1.5 italic comic sans,chalkboard,tscu_comic,fantasy;color:hotpink;']);
 })();
 
 
-const REGEX_ACAO = new RegExp(/(andar|for[cç]a)(?:=(\d+))?/);//[1] contém o tipo, [2] a quantidade
+const REGEX_ACAO = new RegExp(/(for[c\xE7]a|andar)(?:=(\d+))?/);//[1] contém o tipo, [2] a quantidade
 
 /**
  * Cria um novo "botão" no menu e
  * uma caixa de texto para o novo tipo de submissão.
  */
-function initSistema(){
+function initSistema () {
 	const ID_INPUT_TEXT    = 'hard_acao_';
 	const ID_SUBMIT_BUTTON = 'menu_submeter_hard_';
 
-	$("div.ide-menu .ide-menu-item:nth-child(4)").each(function(){
+	$('div.ide-menu .ide-menu-item:nth-child(4)').each( function () {
 		const menu = $(this).find("ul.dropdown-menu");///barra de menu
 		const exercicio_id = menu.attr("aria-labelledby").match(/\d+$/)[0];
 
 		///Novos elementos para a página:
-		const texto = '<li>'+ '<a>' + `<input type="text" class="form-control" style="display:initial;" id="${ID_INPUT_TEXT}${exercicio_id}" value="andar" title="andar|forca" required>&nbsp;` + '</a>' +'</li>';
+		const texto = '<li>'+ '<a>' + `<input type="text" class="form-control" style="display:initial;" id="${ID_INPUT_TEXT}${exercicio_id}" value="andar" title="'andar' ou 'força'" required>&nbsp;` + '</a>' +'</li>';
 		const botao = $( '<li>' +`<a href="#" id=${ID_SUBMIT_BUTTON}${exercicio_id}>` + '<span style="float: left">Submeter</span>' + '<span style="float: right;color:#AAA">F4</span>' + '<span style="padding: .1em .4em .7em"><i class="fa fa-bolt" style="color:#D9534F;"/>&nbsp;Katiau</span>' + '&nbsp;' + '</a>' + '</li>' );
 
-		const submeter_hard = () => {///Ação do evento de click do novo botão de submissão
-			const acao = $('#'+ ID_INPUT_TEXT + exercicio_id).val();
-			euQuero(acao, exercicio_id);
+		const submeter_hard = () => {///ação do evento de click do novo botão de submissão
+			const acaoDesejada = $('#'+ ID_INPUT_TEXT + exercicio_id).val().trim().toLowerCase();
+			euQuero(acaoDesejada, exercicio_id);
 		};
 
 		///Adicionando o evento de tecla de atalho para ativar o 'submeter_hard'
-		$(`div[id^=codigo_fonte][id$=${exercicio_id}]`).keydown(function(e){// $("#codigo_fonte_1_" + exercicio_id)
+		$(`div[id^=codigo_fonte][id$=${exercicio_id}]`).keydown( function ( e ) {// $("#codigo_fonte_1_" + exercicio_id)
 			if(e.which === 115){/// F4
 				e.preventDefault();
 				e.stopImmediatePropagation();
@@ -62,43 +62,43 @@ function initSistema(){
  * @param {String} acao - A ação que as cartas deverão receber; casa com a RegEx 'REGEX_ACAO'.
  * @param {String} idExercicio - (opcional) id da questão que será submetida.
  */
-function euQuero(acao, idExercicio=exercicio_id){
-	if(!acao || !REGEX_ACAO.test(acao)) return;
-	const cardsSelector = "#block_result_" + idExercicio + " div.card";
+function euQuero ( acao='', idExercicio=exercicio_id ) {
+	if(!REGEX_ACAO.test(acao)) return;
+	const exerciseSelector = '#block_result_' + idExercicio;
 
 	const buscarEClicar = () => {
-		const cardFound = findCardWithMaxValue(acao, cardsSelector);
-		const card = $(cardsSelector + ` > span[data-cartaid=${cardFound.id}]`).parent()
-		card.css('z-index', 100);///Traz para o topo
-		card.click();///"Clica" na carta encontrada
+		const cardFound = findCardWithMaxValue(acao.replace('ç','c'), `${exerciseSelector} [data-cartaid]`);
+		const card = $(`${exerciseSelector} span[data-cartaid=${cardFound.id}]`).parent();
+		card.css('z-index', 100);///traz para o topo
+		card.click();///"clica" na carta encontrada
 	};
 
-	$('#submeter_' + idExercicio).trigger('click');///Submeter
-	setTimeout(buscarEClicar, 3500);///Esperar 3,5 segundos e executa o 'buscarEClicar'
+	$('#submeter_' + idExercicio).trigger('click');///submeter
+	setTimeout(buscarEClicar, 3500);///esperar 3,5 segundos e executar o 'buscarEClicar'
 }
 
 /**
  * Utilizado para procurar a carta que
  * possui maior valor associado de acordo com a acao dada.
  * @param {String} tipoAcao - A ação buscada, deve ser "andar" ou "forca".
- * @param {String} selector - Seletor para as cartas.
+ * @param {String} selector - Seletor para as cartas do exercício corrente.
  * @return {Object} - Objeto com as keys: "acao", "max" e "id" (da carta encontrada).
  */
-function findCardWithMaxValue(tipoAcao, selector){
-	const dados = { max:-1 };
+function findCardWithMaxValue ( tipoAcao, selector ) {
+	const arrOfSelected = Array.from($(selector));
 
-	$(selector).each(function(){
-		const card = $(this);
-		const id_card   = card.find("span").data("cartaid");
-		const acao_card = card.find("span").data("acao");
-		const [, key, value] = acao_card.match(REGEX_ACAO);
-		const valor_card = Number(value);
+	const returnLarger= ( acc, card ) => {
+		let id_card   = card.getAttribute('data-cartaid');
+		let acao_card = card.getAttribute('data-acao');
+		let [, key, value] = acao_card.match(REGEX_ACAO);
+		let valor_card = Number(value);
 
-		if((key !== tipoAcao) || !(valor_card > dados.max)) return true;///next
-		Object.assign(dados, {acao:key, max:valor_card, id:id_card});
-	});
+		return	((key === tipoAcao) && (valor_card > acc.max))
+				? { max:valor_card, id:id_card, acao:key }
+				: acc ;
+	}
 
-	return dados;
+	return arrOfSelected.reduce(returnLarger, { max:-1 });
 }
 
 
