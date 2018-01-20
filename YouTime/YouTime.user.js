0e4dc4cff84ba860709abd1148114f03509897fe
Micael Levi <mllc@icomp.ufam.edu.br>
2018-01-20 18:56:05 -0400

    fix lint issues

--- YouTime/YouTime.user.js
+++ YouTime/YouTime.user.js
@@ -15,58 +15,56 @@
 
 // (c) style based on "Simple YouTube MP3 Button" by Arari
 
-function ytime_savetime() {
-    const playerButton = document.getElementsByClassName('ytp-play-button ytp-button')[0];
-    if (!playerButton) return;
+function ytimeSavetime() {
+  const playerButton = document.getElementsByClassName('ytp-play-button ytp-button')[0];
+  if (!playerButton) return;
 
-    const ytime_videoIsPaused = p => p.getElementsByTagName('path')[0].getAttribute('d').length > 60;
+  const ytimeVideoIsPaused = p => p.getElementsByTagName('path')[0].getAttribute('d').length > 60;
 
-    if ( !ytime_videoIsPaused(playerButton) ) playerButton.click();
+  if ( !ytimeVideoIsPaused(playerButton) ) playerButton.click();
 
-    const currTimeAsString = document.getElementsByClassName('ytp-time-current')[0].innerHTML;
-    if (!currTimeAsString) return;
-    const currTimeAsQueryString = currTimeAsString.replace(/(?:(\d+):)?(\d+):(\d+)/, (_, hr, min, seg) => `&t=${hr||0}h${min}m${seg}s`);
-    const newLocationHref = window.location.href.replace(/&t=[^&]+/, '') + currTimeAsQueryString;
-    window.location.replace(newLocationHref);
+  const currTimeAsString = document.getElementsByClassName('ytp-time-current')[0].innerHTML;
+  if (!currTimeAsString) return;
+
+  const currTimeAsQueryString = currTimeAsString.replace(/(?:(\d+):)?(\d+):(\d+)/, (_, hr = 0, min, seg) => `&t=${hr}h${min}m${seg}s`);
+  const newLocationHref = window.location.href.replace(/&t=[^&]+/, '') + currTimeAsQueryString;
+  window.location.replace(newLocationHref);
 }
 
 // ----------------- for new YouTube Layout ----------------- //
 function polymerInject() {
+  // create button
+  const buttonDiv = document.createElement('div');
+  buttonDiv.id = 'ytime-parentbutton';
+  buttonDiv.style.width = '100%';
+  buttonDiv.style.textAlign = 'center';
 
-    // create button
-    const buttonDiv = document.createElement('div');
-    buttonDiv.id = 'ytime-parentbutton';
-    buttonDiv.style.width = '100%';
-    buttonDiv.style.textAlign = 'center';
+  const saveTimeButton = document.createElement('button');
+  buttonDiv.appendChild(saveTimeButton);
+  saveTimeButton.appendChild( document.createTextNode('Save Current Time') );
 
-    const saveTimeButton = document.createElement('button');
-    buttonDiv.appendChild(saveTimeButton);
-    saveTimeButton.appendChild( document.createTextNode('Save Current Time') );
+  saveTimeButton.style.width = '100%';
+  saveTimeButton.style.backgroundColor = '#0f4873';
+  saveTimeButton.style.color = 'var(--yt-subscribe-button-text-color)';
+  saveTimeButton.style.textAlign = 'center';
+  saveTimeButton.style.padding = '4px 0';
+  saveTimeButton.style.marginTop = '5px';
+  //saveTimeButton.style.fontSize = '14px';
+  saveTimeButton.style.border = '0';
+  saveTimeButton.style.cursor = 'pointer';
+  saveTimeButton.style.borderRadius = '2px';
+  saveTimeButton.style.fontFamily = 'Roboto, Arial, sans-serif';
+  saveTimeButton.onclick = ytimeSavetime;
 
-    //saveTimeButton.className = 'style-scope ytd-button-renderer';
-    saveTimeButton.style.width = '100%';
-    saveTimeButton.style.backgroundColor = '#0f4873';
-    saveTimeButton.style.color = 'var(--yt-subscribe-button-text-color)';
-    saveTimeButton.style.textAlign = 'center';
-    saveTimeButton.style.padding = '4px 0';
-    saveTimeButton.style.marginTop = '5px';
-    //saveTimeButton.style.fontSize = '14px';
-    saveTimeButton.style.border = '0';
-    saveTimeButton.style.cursor = 'pointer';
-    saveTimeButton.style.borderRadius = '2px';
-    saveTimeButton.style.fontFamily = 'Roboto, Arial, sans-serif';
-    saveTimeButton.onclick = ytime_savetime;
+  // Find and add to target (below "Subscribe" button)
+  const targetElements = document.querySelectorAll('[id=subscribe-button]');
+  targetElements.forEach((targetElement) => {
+    if (targetElement.className.indexOf('ytd-video-secondary-info-renderer') > -1) targetElement.appendChild(buttonDiv);
+  });
 
-    // Find and add to target (below "Subscribe" button)
-    const targetElements = document.querySelectorAll("[id=subscribe-button]");
-    targetElements.forEach(targetElement => {
-        if (targetElement.className.indexOf('ytd-video-secondary-info-renderer') > -1) targetElement.appendChild(buttonDiv);
-    });
-
-    // Fix hidden description bug
-    const descriptionBox = document.querySelectorAll('ytd-video-secondary-info-renderer')[0];
-    if(descriptionBox.className.indexOf('loading') > -1) descriptionBox.classList.remove('loading');
-
+  // Fix hidden description bug
+  const descriptionBox = document.querySelectorAll('ytd-video-secondary-info-renderer')[0];
+  if (descriptionBox.className.indexOf('loading') > -1) descriptionBox.classList.remove('loading');
 }
 
 
@@ -119,40 +117,36 @@ function run(){
 
     var buttonDiv = document.createElement("div");
     buttonDiv.id = 'ytime-parentbutton';
-		buttonDiv.className = "yt-uix-button yt-uix-button-default";
-    buttonDiv.style.height = "23px";
-    buttonDiv.style.marginLeft = "28px";
-    buttonDiv.style.paddingBottom = "1px";
-    buttonDiv.onclick = ytime_savetime;
+    buttonDiv.className = 'yt-uix-button yt-uix-button-default';
+    buttonDiv.style.height = '23px';
+    buttonDiv.style.marginLeft = '28px';
+    buttonDiv.style.paddingBottom = '1px';
+    buttonDiv.onclick = ytimeSavetime;
 
-    document.getElementById("watch7-user-header").appendChild(buttonDiv);
+    document.getElementById('watch7-user-header').appendChild(buttonDiv);
 
-    var childButton = document.createElement("span");
+    const childButton = document.createElement('span');
     buttonDiv.appendChild(childButton);
 
     childButton.appendChild(document.createTextNode('Save Current Time'));
-    childButton.className = "yt-uix-button-content";
-    childButton.style.lineHeight = "25px";
-    childButton.style.fontSize = "12px";
-
+    childButton.className = 'yt-uix-button-content';
+    childButton.style.lineHeight = '25px';
+    childButton.style.fontSize = '12px';
   }
-
 }
 
 
 // ----------------- injecting ----------------- //
-if(document.getElementById('polymer-app') || document.getElementById('masthead') || window.Polymer) {
-
+if (document.getElementById('polymer-app') || document.getElementById('masthead') || window.Polymer) {
   setInterval(() => {
-    if(window.location.href.indexOf('watch?v=') < 0) return false;
-    if(document.getElementById('count') && document.getElementById('ytime-parentbutton') === null) polymerInject();
+    if (window.location.href.indexOf('watch?v=') < 0) return false;
+    if (document.getElementById('count') && document.getElementById('ytime-parentbutton') === null) polymerInject();
+    return true;
   }, 100);
-
 } else {
-
   setInterval(() => {
-    if(window.location.href.indexOf('watch?v=') < 0) return false;
-    if(document.getElementsByClassName('watch-view-count').length && document.getElementById('ytime-parentbutton') === null) standardInject();
+    if (window.location.href.indexOf('watch?v=') < 0) return false;
+    if (document.getElementsByClassName('watch-view-count').length && document.getElementById('ytime-parentbutton') === null) standardInject();
+    return true;
   }, 100);
-
 }
