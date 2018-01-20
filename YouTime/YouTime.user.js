6179738d355d7e78a66f33cb06bc7af197066890
Micael Levi <mllc@icomp.ufam.edu.br>
2018-01-20 18:56:29 -0400

    rm useless code

--- YouTime/YouTime.user.js
+++ YouTime/YouTime.user.js
@@ -70,52 +70,10 @@ function polymerInject() {
 
 // ----------------- for old YouTube Layout ----------------- //
 function standardInject() {
-  var pagecontainer=document.getElementById('page-container');
-  if (!pagecontainer) return;
-  if (/^https?:\/\/www\.youtube.com\/watch\?/.test(window.location.href)) run();
+  if ( !document.getElementById('page-container') ) return;
 
-  var isAjax=/class[\w\s"'-=]+spf\-link/.test(pagecontainer.innerHTML);
-  var logocontainer=document.getElementById('logo-container');
-  if (logocontainer && !isAjax) { // fix for blocked videos
-    isAjax=(' '+logocontainer.className+' ').indexOf(' spf-link ')>=0;
-  }
-
-  var content=document.getElementById('content');
-  if (isAjax && content) { // Ajax UI
-    var mo=window.MutationObserver||window.WebKitMutationObserver;
-    if(typeof mo!=='undefined') {
-      var observer=new mo(function(mutations) {
-        mutations.forEach(function(mutation) {
-          if(mutation.addedNodes!==null) {
-            for (var i=0; i<mutation.addedNodes.length; i++) {
-              if (mutation.addedNodes[i].id=='watch7-container' ||
-              mutation.addedNodes[i].id=='watch7-main-container') { // old value: movie_player
-                run();
-                break;
-              }
-            }
-          }
-        });
-      });
-      observer.observe(content, {childList: true, subtree: true}); // old value: pagecontainer
-    } else { // MutationObserver fallback for old browsers
-      pagecontainer.addEventListener('DOMNodeInserted', onNodeInserted, false);
-    }
-  }
-}
-
-function onNodeInserted(e) {
-  if (e && e.target && (e.target.id=='watch7-container' ||
-  e.target.id=='watch7-main-container')) { // old value: movie_player
-    run();
-  }
-}
-
-function run(){
-
-  if(!document.getElementById('ytime-parentbutton') && window.location.href.substring(0, 25).indexOf("youtube.com") > -1 && window.location.href.indexOf("watch?v=") > -1){
-
-    var buttonDiv = document.createElement("div");
+  if (!document.getElementById('ytime-parentbutton') && window.location.href.substring(0, 25).indexOf('youtube.com') > -1 && window.location.href.indexOf('watch?v=') > -1) {
+    const buttonDiv = document.createElement('div');
     buttonDiv.id = 'ytime-parentbutton';
     buttonDiv.className = 'yt-uix-button yt-uix-button-default';
     buttonDiv.style.height = '23px';
