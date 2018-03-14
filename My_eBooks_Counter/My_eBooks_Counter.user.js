c2be46fa414b281f83f0b1969e811cb50a66cd62
Micael Levi <mllc@icomp.ufam.edu.br>
2018-03-13 21:21:34 -0400

    atualizado

--- My_eBooks_Counter/My_eBooks_Counter.user.js
+++ My_eBooks_Counter/My_eBooks_Counter.user.js
@@ -3,10 +3,8 @@
 // @description Adiciona no título a quantidade de livros adquiridos
 // @namespace   packtpub
 // @author      Micael Levi
-// @language    pt-br
 // @include     *://www.packtpub.com/account/my-ebooks
-// @version     0.23-1
+// @version     0.13-3
 // @grant       none
-// @run-at		document-end
 // ==/UserScript==
-document.getElementsByTagName('h1')[1].innerHTML += `(${$('div[class="product-line unseen"').length})`
+document.getElementsByTagName('h1')[1].innerHTML += `(${document.querySelectorAll('div[class="product-line unseen"').length})`
