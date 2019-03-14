f196dc3139968baca1d8ac39f17ec0fd8d777d91
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-03-13 23:20:27 -0400

    rm: hardcoded colunas selecionadas

--- PergamumUFAM+/PergamumUFAM+.user.js
+++ PergamumUFAM+/PergamumUFAM+.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name     PergamumUFAM+
+// @name        PergamumUFAM+
 // @description Adiciona um botão para (tentar) renovar todos os títulos emprestados e um botão para agendar no Todoist a próxima renovação.
-// @version  0.20-8
-// @include  *://pergamum.ufam.edu.br/pergamum/biblioteca_s/meu_pergamum/index.php*
-// @grant    none
-// @run-at   document-end
+// @version     0.13-3
+// @compatible firefox
+// @include    *://pergamum.ufam.edu.br/pergamum/biblioteca_s/meu_pergamum/index.php*
+// @grant      none
+// @run-at     document-end
 // ==/UserScript==
-
 
 const unsafeWindow = window.wrappedJSObject;
 
 (function (fetch){
 
   //#region private
-  const TODOIST_TOKEN = '---------MY-----API-------TOKEN---------'; // https://todoist.com/prefs/integrations
+  const TODOIST_TOKEN = '---------MY-----API-------TOKEN---------'; // https://todoist.com/prefs/integrations;
   //#endregion
 
   //#region DOM injection
@@ -38,9 +38,8 @@ const unsafeWindow = window.wrappedJSObject;
     btn.style.opacity = 1 - 0.5 * btn.disabled;
   }
 
-  // WORKING ONLY ON TAMPERMONKEY:
   // Todoist REST API Reference https://developer.todoist.com/rest/v8
-  function createTaskOnTodoist(content, dueDate, priority = 2) {
+  function createTaskOnTodoist(content, dueDate, priority = 4) {
     // UUID v4 generator non RFC4122 compliant
     function uuidgen() {
       const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
@@ -61,7 +60,7 @@ const unsafeWindow = window.wrappedJSObject;
         'Authorization': `Bearer ${TODOIST_TOKEN}`
       },
       body: JSON.stringify(data)
-    }).then((resp) => { toggleFeedbackButton(); }).catch(console.info);
+    }).then((resp) => { toggleFeedbackButton(); alert('tarefas registradas!'); }).catch(console.error);
   }
 
   function recuperarDatasDevolucao() {
@@ -71,7 +70,8 @@ const unsafeWindow = window.wrappedJSObject;
       return new Date(dateStr[2], dateStr[1] - 1, dateStr[0]); // ISO YYYY-MM-DDTHH:MM:SS
     };
 
-    return [ nodes[3], nodes[6], nodes[9], nodes[12] ].map(node => node2Date(node));
+    return nodes.filter((_, idx) => idx % 3 === 0) // apenas colunas de `Data de Devolução`
+                .map(node => node2Date(node));
   }
 
   function enviarFeedback() {
@@ -85,4 +85,4 @@ const unsafeWindow = window.wrappedJSObject;
     return createTaskOnTodoist('renovar empréstimos pergamum', dueDate);
   }
 
-}(unsafeWindow.fetch));
+}(fetch || unsafeWindow.fetch));
